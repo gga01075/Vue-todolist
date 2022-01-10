30cdc18ebea09bf73fe8232c4c93e44a0bcbce00
gga01075 <72422020+gga01075@users.noreply.github.com>
2022-01-10 23:15:43 +0900

    create todolist

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keyup.enter="addTodo">
+        <span class="addContainer" v-on:click="addTodo">
+             <i class="addBtn fa fa-plus" aria-hidden="true"></i>
+        </span>
+    </div>
+</template>
+<script>
+
+
+export default({
+    data(){
+        return{
+            newTodoItem: ''
+        }
+    },
+    methods: {
+        addTodo(){
+            if(this.newTodoItem!==""){
+            // console.log(this.newTodoItem);
+            // trim은 string의 앞뒤 공백을 제거
+            var value = this.newTodoItem && this.newTodoItem.trim();
+            // 입력받은 텍스트를 로컬 스토리지의 setItem()API를 이용하여 저장
+            // API형식은 키,값 형태이며 저장 기능을 단순하게 하기 위해 키,값 모두 입력받은 텍스트로 지정
+            this.$emit('addTodo',value);
+            this.clearInput();
+            }
+            /* 
+            로컬 스토리지에 저장된 것을 확인하려면
+            크롬 개발자 도구의 [Application -> Local Storage -> http://localhost:8080]
+            */
+        },
+        
+        clearInput(){
+            this.newTodoItem = '';
+        }
+    }
+})
+</script>
+<style>
+    input:focus{
+        outline: none;
+    }
+    .inputBox{
+        background: white;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 5px;
+    }
+    .inputBox input{
+        border-style: none;
+        font-size: 0.9rem;
+    }
+    .addContainer{
+        float: right;
+        background: linear-gradient(to right, #6478fb, #8763fb);
+        display: inline-block;
+        width: 3rem;
+        border-radius: 0 5px 5px 0;
+    }
+    .addBtn{
+        color: white;
+        vertical-align: middle;
+    }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <section>
+            <ul>
+               <li v-for="(todoItem,index) in propsdata" class="shadow">
+                   <i class="checkBtn fa fa-check" aria-hidden="true"></i>
+                   {{todoItem}}
+                   <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+                    <i class="far fa-trash-alt" aria-hidden="true"></i>
+                   </span>
+                   
+              </li>
+            </ul>
+        </section>
+
+    </div>
+</template>
+<script>
+        /* 
+          Todo리스트 리스트에 나타내기
+            1단계 : 로컬 스토리지 데이터를 뷰 데이터에 저장
+            2단계 : 뷰 데이터의 아이템 개수만큼 리스트 아이템 표시
+        */
+
+export default({
+   props: ['propsdata'],
+
+   methods: {
+       /* 
+       삭제기능
+        1단계: 선택한 할일을 뷰에서 인식
+        2단계: 선택한 할일을 로컬 스토리지에서 삭제
+        3단계: 선택한 할일을 뷰 데이터에서 삭제 
+       */
+    removeTodo(todoItem, index) {
+      this.$emit('removeTodo', todoItem, index);
+    }
+   },
+
+})
+</script>
+<style>
+    ul{
+        list-style-type: none;
+        padding-left: 0;
+        margin-top: 0;
+        text-align: left;
+    }
+    li{
+        display: flex;
+        min-height: 50px;
+        height: 50px;
+        line-height: 50px;
+        margin: 0.5rem 0;
+        padding: 0 0.9rem;
+        background: white;
+        border-radius: 5px;
+    }
+    .checkBtn{
+        line-height: 45px;
+        color: #62acde;
+        margin-right: 5px;
+    }
+    .removeBtn{
+        margin-left: auto;
+        color: #de4343;
+    }
+</style>
